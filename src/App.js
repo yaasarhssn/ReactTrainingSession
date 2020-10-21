b19af997aedc7590c71efa7e19a37e3dbee12a6b
yaasarhssn <68842700+yaasarhssn@users.noreply.github.com>
2020-10-21 10:13:09 +0530

    first commit with react task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'; 
+import { BrowserRouter as Router, Route, Link, Switch,NavLink } from 'react-router-dom'; 
+import Home from './component/Home'; 
+import About from './component/About-us'; 
+import Contact from './component/Contact'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router> 
+           <div className="App"> 
+            <ul className="App-header"> 
+              <li> 
+                <Link to="/">Home</Link> 
+              </li> 
+              {/* <li> 
+                <Link to="/about">About Us</Link> 
+              </li>  */}
+              <li> 
+                <Link to="/contact">Contact Us</Link> 
+              </li> 
+              <li>
+            <NavLink to="/about" exact activeStyle={
+              { color:'green' }
+            }>About(using NavLink)
+            </NavLink>
+          </li>
+            </ul> 
+            <Switch> 
+              <Route exact path='/' activeStyle={
+              { color:'green' } }component={Home}></Route> 
+              <Route exact path='/about' exact strict component={About}></Route> 
+              <Route exact path='/contact' component={Contact}></Route> 
+            </Switch> 
+          </div> 
+       </Router> 
     </div>
   );
 }
